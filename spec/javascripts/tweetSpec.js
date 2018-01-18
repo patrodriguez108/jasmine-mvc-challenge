4cb2818393468e8f603330493b4b8c4dc2ade896
Patrick <pat.rodriguez108@gmail.com>
2018-01-18 16:05:55 -0600

    Add spec test for alert if the tweet is too long

--- spec/javascripts/tweetSpec.js
+++ spec/javascripts/tweetSpec.js
@@ -3,6 +3,8 @@ describe("a tweet", function() {
 
   beforeEach(function() {
     viableTweet = new Tweet("animelover12", "I love DBZ XD");
+    longTweet = new Tweet("this_frickin_dude", "Allow me to tell you about the wondrous world of Attack on Titan. It tells about a world of soldiers who fight a futile war, a war that is impossible to win, yet they keep on fighting. They fight for survival. It is a great metaphor for the cycle of war, and shows the perspective of folks who are fighting against people whose technology is too advanced for anyone to beat them.")
+    spyOn(window, 'alert');
   });
 
   it("has content", function() {
@@ -15,5 +17,10 @@ describe("a tweet", function() {
 
   it("is less than 140 characters", function() {
     expect(viableTweet.isLessThanOneFortyCharacters()).toBe(true)
+  });
+
+  it("alerts if the tweet is too long", function() {
+    longTweet.characterLength();
+    expect(window.alert).toHaveBeenCalledWith("Tweet must be less than 140 characters")
   })
 });

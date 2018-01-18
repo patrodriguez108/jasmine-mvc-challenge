4fa9d40e04ec73cd52082cffa33f46d68707bb68
Patrick <pat.rodriguez108@gmail.com>
2018-01-18 15:08:35 -0600

    Convert new tweet form to form_for

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,12 +4,14 @@ class TweetsController < ApplicationController
   end
 
   def create
+    p "_________________"
     @tweet = Tweet.new(content: params[:content], username: params[:username])
     if @tweet.save
-      respond_to do |f|
-        f.html { redirect_to root_url }
-        f.js
-      end
+      redirect_to root_url
+      # respond_to do |f|
+      #   f.html { redirect_to root_url }
+      #   f.js
+      # end
     else
       render plain: "Error saving tweet", status: 422
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :tweets, only: [:index, :create]
 
   root 'tweets#index'
+  post '/tweets', to: 'tweets#create'
 end

ad8a5ea6259b21b909c6c6328d8429ff691b66ed
Patrick <pat.rodriguez108@gmail.com>
2018-01-18 15:52:40 -0600

    Add 140 character limit to js file

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -2,3 +2,13 @@ var Tweet = function(username, content) {
 	this.username = username;
 	this.content = content
 };
+
+Tweet.protoype.isLessThanOneFortyCharacters = function() {
+	return this.content.length < 140
+};
+
+Tweet.prototype.characterLength = function() {
+	if (this.isLessThanOneFortyCharacters) {
+		alert("Tweet must be less than 140 characters")
+	}
+}
